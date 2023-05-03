0a3c85f0f6a6f340ff1c3b5c78986ac7ac3c8ce4
RayanShashmin <132475422+RayanShashmin@users.noreply.github.com>
2023-05-04 00:10:11 +0530

    First commit
    first date 5/4/2023

---
+++ src/components/TopicBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '/topicbox.css'
+
+export default function TopicBox() {
+  return (
+    <div>
+        <div className="topicBox">
+            <span>This is the Topic</span>
+        </div>
+    </div>
+  )
+}
